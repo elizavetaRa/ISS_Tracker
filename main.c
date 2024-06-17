9f2a8d35ec80e440e48bad5db5636a8f9a145a56
Elizaveta Ragozina <elizaveta.ragozina@cern.ch>
2024-06-17 18:50:52 +0200

    Add async behavior to the API calls

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 #include <math.h>
 #include <glib.h>
 #include <gtk/gtk.h>
-#include <gdk/gdkkeysyms.h>
+
 #include <json-c/json.h>
 #include <curl/curl.h>
 #include <stdlib.h>
@@ -24,59 +24,98 @@ static char *response = NULL;
 static size_t WriteCallback(void *contents, size_t size, size_t nmemb, void *userp)
 {
     size_t realsize = size * nmemb;
-    char **response_ptr = (char **)userp;
-
-    *response_ptr = strndup(contents, realsize);
-
+    response = strndup(contents, realsize);
     return realsize;
 }
 
-// API Call to get the current latitude and the longitude of ISS
-static gboolean getIssLocation()
+// Function to perform the CURL request to ISS API in a separate thread
+static void getIssApiDataTask(GTask *task, gpointer source_object, gpointer task_data, GCancellable *cancellable)
 {
+    CURL *curl;
+    CURLcode res;
+
     curl_global_init(CURL_GLOBAL_DEFAULT);
-    CURL *curl = curl_easy_init();
+    curl = curl_easy_init();
     if (curl)
     {
-        CURLcode res;
-
         curl_easy_setopt(curl, CURLOPT_URL, "http://api.open-notify.org/iss-now.json");
         curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
-        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &response);
+        curl_easy_setopt(curl, CURLOPT_TIMEOUT, 10L); // Add timeout of 5 seconds
+
         res = curl_easy_perform(curl);
-
-        if (res == CURLE_OK && response)
+        if (res != CURLE_OK)
         {
-            struct json_object *parsed_json;
-            struct json_object *iss_position;
-            struct json_object *latitude;
-            struct json_object *longitude;
+            g_task_return_error(task, g_error_new_literal(G_IO_ERROR, G_IO_ERROR_FAILED, curl_easy_strerror(res)));
+        }
+        else
+        {
+            g_task_return_pointer(task, response, g_free);
+        }
 
-            parsed_json = json_tokener_parse(response);
+        curl_easy_cleanup(curl);
+    }
+    else
+    {
+        g_task_return_error(task, g_error_new_literal(G_IO_ERROR, G_IO_ERROR_FAILED, "Failed to initialize CURL"));
+    }
+
+    curl_global_cleanup();
+}
+
+// Function to handle the completion of the async task
+static void onIssApiDataTaskComplete(GObject *source_object, GAsyncResult *res, gpointer user_data)
+{
+    GTask *task = G_TASK(res);
+    if (g_task_had_error(task))
+    {
+        g_warning("API request failed");
+    }
+    else
+    {
+        char *response = g_task_propagate_pointer(task, NULL);
+        if (response)
+        {
+            struct json_object *parsed_json = json_tokener_parse(response);
             if (parsed_json == NULL)
             {
                 fprintf(stderr, "Failed to parse JSON response\n");
+                return;
             }
+
+            struct json_object *iss_position;
+            struct json_object *latitude;
+            struct json_object *longitude;
 
             if (json_object_object_get_ex(parsed_json, "iss_position", &iss_position))
             {
                 if (json_object_object_get_ex(iss_position, "latitude", &latitude) &&
                     json_object_object_get_ex(iss_position, "longitude", &longitude))
                 {
-
-                    // Get latitude and longitude as strings
                     const char *lat_str = json_object_get_string(latitude);
                     const char *lon_str = json_object_get_string(longitude);
 
-                    // Convert latitude and longitude to gdouble (double precision)
                     gdouble lat = g_ascii_strtod(lat_str, NULL);
                     gdouble lon = g_ascii_strtod(lon_str, NULL);
 
-                    // Convert gdouble to float
                     float_lat = (float)lat;
                     float_lon = (float)lon;
 
                     printf("Float Latitude: %f, Float Longitude: %f\n", float_lat, float_lon);
+
+                    if (track)
+                    {
+                        // Update existing track
+                        osm_gps_map_track_add_point(track, osm_gps_map_point_new_degrees(float_lat, float_lon));
+                    }
+                    else
+                    {
+                        // Create new track if not already created
+                        track = osm_gps_map_track_new();
+                        osm_gps_map_set_center(OSM_GPS_MAP(map), float_lat, float_lon);
+                        osm_gps_map_set_zoom(OSM_GPS_MAP(map), 3);
+                        g_object_set(track, "editable", FALSE, NULL);
+                        osm_gps_map_track_add(OSM_GPS_MAP(map), track);
+                    }
 
                     // Update label with current ISS location
                     gchar *location_text = g_strdup_printf("Current ISS Location: Latitude: %f, Longitude: %f", float_lat, float_lon);
@@ -94,45 +133,19 @@ static gboolean getIssLocation()
             }
 
             json_object_put(parsed_json);
-            free(response);
-            response = NULL; // Reset response pointer after freeing memory
+            g_free(response);
         }
-        else
-        {
-            fprintf(stderr, "CURL error: %s\n", curl_easy_strerror(res));
-        }
-
-        curl_easy_cleanup(curl);
     }
-    else
-    {
-        fprintf(stderr, "Failed to initialize CURL\n");
-    }
-
-    return TRUE;
 }
 
-// Function to create a new map track with ISS location
-// or add a new location point to an excisting track
-static gboolean addIssLocationPoint()
+// Function to initiate the async API call
+static gboolean getIssLocationAsync(gpointer user_data)
 {
-    getIssLocation();
+    GTask *task = g_task_new(NULL, NULL, onIssApiDataTaskComplete, NULL);
 
-    // if no track is created, create new and set the starting tracking point as map center
-    if (!track)
-    {
-        track = osm_gps_map_track_new();
-        osm_gps_map_set_center(OSM_GPS_MAP(map), float_lat, float_lon);
-        osm_gps_map_set_zoom(OSM_GPS_MAP(map), 3);
-        g_object_set(track, "editable", FALSE, NULL);
-        osm_gps_map_track_add(OSM_GPS_MAP(map), track);
-    }
-
-    // add newly retrieved tracking point to the map
-    point = osm_gps_map_point_new_degrees(float_lat,
-                                          float_lon);
-    osm_gps_map_track_add_point(track, point);
-
+    // Runs task in another thread and when it return, callback will be invoced in GMainContext
+    g_task_run_in_thread(task, getIssApiDataTask);
+    g_object_unref(task);
     return TRUE;
 }
 
@@ -195,8 +208,8 @@ static void activate(GtkApplication *app, gpointer user_data)
     gtk_widget_show_all(window);
 
     // Initially call ISS location API and then call every 5 seconds
-    addIssLocationPoint();
-    g_timeout_add_seconds(5, addIssLocationPoint, NULL);
+    getIssLocationAsync(NULL);
+    g_timeout_add_seconds(5, getIssLocationAsync, NULL);
 }
 
 int main(int argc, char **argv)
